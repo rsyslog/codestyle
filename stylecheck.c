1b0ae01d2bc6057d880a6cff24e072d7ce5e57ca
Rainer Gerhards <rgerhards@adiscon.com>
2016-04-28 11:42:37 +0200

    emit more message and warning, if even more could be found

--- stylecheck.c
+++ stylecheck.c
@@ -7,7 +7,7 @@ check_file(int *had_err, const char *fn)
 	int ln_nbr;
 	char ln[4096];
 	int msgs = 0;
-	int max_msgs = 2;
+	int max_msgs = 20;
 	
 	if ((fp = fopen (fn, "r")) == NULL) {
 		perror (fn);
@@ -18,16 +18,20 @@ check_file(int *had_err, const char *fn)
 	ln_nbr = 1;
 	while (fgets (ln, sizeof(ln), fp) != NULL) {
 		if (ln[0] == ' ' && ln[1] != '*') {
-			if (msgs < max_msgs) {
-				fprintf(stderr, "error: %s:%d: invalid indention (SP at column 1):"
-					"\n%s\n", fn, ln_nbr, ln);
-				*had_err = 1;
-				++msgs;
+			if(msgs == max_msgs) {
+				fprintf(stderr, "reached max %d messages, not reporting more\n",
+					max_msgs);
+				goto done;
 			}
+			fprintf(stderr, "error: %s:%d: invalid indention (SP at column 1):"
+				"\n%s\n", fn, ln_nbr, ln);
+			*had_err = 1;
+			++msgs;
 		}
 		++ln_nbr;
 	}
 
+done:
 	fclose (fp);
 }
 
