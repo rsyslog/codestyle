120ad0a3c22fd10b9351556f6027743beb609e98
Jan Gerhards <jgerhards@adiscon.com>
2016-11-28 10:31:56 +0100

    bugfix: line correctly displayed if line too long
    closes https://github.com/rsyslog/codestyle/issues/14

--- tests/chk_linelength.sh
+++ tests/chk_linelength.sh
@@ -11,7 +11,9 @@ void main()
 }
 ' > testfile.c # last line is a TAB!
 ../stylecheck testfile.c &> output
-echo 'error: testfile.c:7: line to long:' | diff - output
+echo 'error: testfile.c:7: line too long:
+	if(( variable1 < variable2 || variable1 = variable2 ) && variable2 > variable2 - variable1 && variable2 != 0 && ( variable1 == 2 || variable1 == 3 ) && (variable2 == 4 || variable 2 == 5)) {
+' | diff - output
 if [ ! $? -eq 0 ]; then
   echo "invalid output generated! Output is:"
   cat output
