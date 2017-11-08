66106ab132660f8ea490e8c0a252ec2ced6670d2
PascalWithopf <pwithopf@adiscon.com>
2017-11-08 10:52:05 +0100

    add test checking length of lines with tabs

---
+++ tests/chk_linelength_tabs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo '#include <stdio.h>
+
+void main()
+{
+	int variable1=2;
+	int variable2=5;
+							if( variable1 < variable2 || variable1 == variable2 || variable1 > variable2) {
+
+	}
+}
+' > testfile.c # last line is a TAB!
+../stylecheck -wf testfile.c &> output
+echo 'error: testfile.c:7: line too long:
+Length of tabs: 49
+							if( variable1 < variable2 || variable1 == variable2 || variable1 > variable2) {
+' | diff - output
+if [ ! $? -eq 0 ]; then
+  echo "invalid output generated! Output is:"
+  cat output
+  exit 1
+fi;
+
+rm testfile.c output
